8e6acb6f3698e13033f69779bbc7773f780813b6
Bielfla27 <gabibielmft@outlook.com>
2022-10-10 21:04:51 -0300

    Criando um crudRepository

---
+++ src/main/java/br/com/alura/spring/data/repository/CargoRepository.java
@@ -0,0 +1,9 @@
+package br.com.alura.spring.data.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CargoRepository extends CrudRepository {
+
+}
