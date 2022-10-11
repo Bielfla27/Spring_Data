e1a0a078706e4b33c6e64d68888d270f1d3ce64b
Bielfla27 <gabibielmft@outlook.com>
2022-10-10 21:23:23 -0300

    Inserindo dados no banco e vendo injeção de dependência

--- src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -1,13 +1,30 @@
 package br.com.alura.spring.data;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.alura.spring.data.orm.Cargo;
+import br.com.alura.spring.data.repository.CargoRepository;
+
 @SpringBootApplication
-public class SpringDataApplication {
+public class SpringDataApplication implements CommandLineRunner{
+	
+	private final CargoRepository repository;
+	
+	public SpringDataApplication(CargoRepository repository) {
+		this.repository = repository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Cargo cargo = new Cargo();
+		cargo.setDescricao("Desenvolvedor Java MV");
+		repository.save(cargo);
 	}
 
 }
--- src/main/java/br/com/alura/spring/data/repository/CargoRepository.java
+++ src/main/java/br/com/alura/spring/data/repository/CargoRepository.java
@@ -3,7 +3,10 @@ package br.com.alura.spring.data.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface CargoRepository extends CrudRepository {
+import br.com.alura.spring.data.orm.Cargo;
 
+@Repository
+public interface CargoRepository extends CrudRepository<Cargo, Integer> { //passo como parâmetro o tipo do objeto e o tipo do ID desse objeto
+
+	
 }
