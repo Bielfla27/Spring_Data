c72f19568be1dfcf9e70d571da68718b1b92a884
Bielfla27 <gabibielmft@outlook.com>
2022-10-12 00:45:08 -0300

    adicionando o método de atualizar no crud

--- src/main/java/br/com/alura/spring/data/service/CrudCargoService.java
+++ src/main/java/br/com/alura/spring/data/service/CrudCargoService.java
@@ -10,6 +10,7 @@ import br.com.alura.spring.data.repository.CargoRepository;
 @Service
 public class CrudCargoService {
 	
+	private Boolean system = true; 
 	private final CargoRepository cargoRepository;
 	
 	public CrudCargoService(CargoRepository cargoRepository) {
@@ -17,7 +18,30 @@ public class CrudCargoService {
 	}
 	
 	public void inicial(Scanner scanner) {
-		salvar(scanner);
+		Scanner opção = new Scanner(System.in);
+		
+		while(system) {
+			System.out.println("Qual ação você quer no cargo");
+			System.out.println("0 -> SAIR");
+			System.out.println("1 -> Salvar cargo");
+			System.out.println("2 -> Atualizar cargo");
+			int action = opção.nextInt();
+			switch (action) {
+			case 0:
+				system = false;
+				break;
+			case 1:
+				salvar(scanner);
+				break;
+			case 2:
+				atualizar(scanner);
+				break;
+			default:
+				System.out.println();
+				System.out.println("Valor invalido, tente algumas das opções sugeridas!");
+				System.out.println();
+			}
+		}
 	}
 	
 	private void salvar(Scanner scanner) {
@@ -27,6 +51,18 @@ public class CrudCargoService {
 		cargo.setDescricao(descricao);
 		cargoRepository.save(cargo);
 		System.out.println("Salvo com successo");
+	}
+
+	private void atualizar(Scanner scanner) {
+		System.out.println("Degite o id que deseja atualizar");
+		Integer id = scanner.nextInt();
+		System.out.println("Descricao do cargo");
+		String descricao = scanner.next();
+		Cargo cargo = new Cargo();
+		cargo.setId(id);
+		cargo.setDescricao(descricao);
+		cargoRepository.save(cargo);
+		System.out.println("Atualizado com successo");
 	}
 
 }
