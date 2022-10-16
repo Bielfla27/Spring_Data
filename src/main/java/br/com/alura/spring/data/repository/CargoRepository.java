5c713c553231b629d2e67f7d4ec5245e010c0c06
Bielfla27 <gabibielmft@outlook.com>
2022-10-16 20:56:25 -0300

    Adiconando a paginação para Cargo

--- src/main/java/br/com/alura/spring/data/repository/CargoRepository.java
+++ src/main/java/br/com/alura/spring/data/repository/CargoRepository.java
@@ -1,12 +1,13 @@
 package br.com.alura.spring.data.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.spring.data.orm.Cargo;
 
 @Repository
-public interface CargoRepository extends CrudRepository<Cargo, Integer> { //passo como parâmetro o tipo do objeto e o tipo do ID desse objeto
+public interface CargoRepository extends PagingAndSortingRepository<Cargo, Integer> { //passo como parâmetro o tipo do objeto e o tipo do ID desse objeto
 
 	
 }
--- src/main/java/br/com/alura/spring/data/service/CrudCargoService.java
+++ src/main/java/br/com/alura/spring/data/service/CrudCargoService.java
@@ -2,9 +2,14 @@ package br.com.alura.spring.data.service;
 
 import java.util.Scanner;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import br.com.alura.spring.data.orm.Cargo;
+import br.com.alura.spring.data.orm.Funcionario;
 import br.com.alura.spring.data.repository.CargoRepository;
 
 @Service
@@ -41,7 +46,7 @@ public class CrudCargoService {
 				atualizar(scanner);
 				break;
 			case 3:
-				vizualizar();
+				vizualizar(scanner);
 				break;
 			case 4:
 				deletar(scanner);
@@ -79,8 +84,16 @@ public class CrudCargoService {
 		System.out.println();
 	}
 	
-	private void vizualizar() {
-		Iterable<Cargo> cargos = cargoRepository.findAll();
+	private void vizualizar(Scanner scanner) {
+		System.out.println("Qual pagina você deseja visualizar");
+		Integer page = scanner.nextInt();
+		
+		Pageable pageable = PageRequest.of(page, 5, Sort.unsorted());
+		Page<Cargo> cargos = cargoRepository.findAll(pageable);
+		
+		System.out.println(cargos);
+		System.out.println("Pagina atual " + cargos.getNumber());
+		System.out.println("Total elemento " + cargos.getTotalElements());
 		cargos.forEach(cargo -> System.out.println(cargo));
 		System.out.println();
 	}
